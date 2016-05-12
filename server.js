d5a52c08d5037c6b99ab11f4446dd77bcba2b79c
Filip Bartoš <filip@bartos.me>
2016-05-12 09:10:10 +0200

    Updated helper to check if cookie exists + minor changes

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import expressHandlebars from 'express-handlebars';
 import cookieParser from 'cookie-parser';
-
 import path from 'path';
 
 
@@ -13,26 +12,17 @@ app.use(express.static('build'));
 
 // set up handlebars
 app.engine('handlebars', expressHandlebars({
-                                            defaultLayout: 'main',
-                                            layoutsDir: path.join(__dirname, 'views/layouts'),
-                                            partialsDir: path.join(__dirname, 'views/partials')
-                                          }));
+                                           defaultLayout: 'main',
+                                           layoutsDir: path.join(__dirname, 'views/layouts'),
+                                           partialsDir: path.join(__dirname, 'views/partials')
+                                         }));
 app.set('view engine', 'handlebars');
 
-    app.get('/', function(req, res){
-    	 res.status(200).render('content',
-          {
-            helpers: {
-              ifNeedToRenderFast: function(options) {
-                if(req.cookies.fastweb && req.cookies.fastweb === 'cache') {
-                  return options.inverse(this);
-                } else {
-                  return options.fn(this);
-                }
-    	        }
-            }
-          });
+app.get('/', function(req, res){
+    res.status(200).render('content', {
+      needToRenderFast: !(req.cookies.fastweb === 'cache')
     });
+});
 
 app.listen(3000, function(){
 console.log('Local server is listening...');
