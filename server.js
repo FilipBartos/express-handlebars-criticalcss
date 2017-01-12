e7a0066da3e0e7c14aa2060a62ae223f1404f905
Filip Bartoš <filip@bartos.me>
2017-01-12 22:19:48 +0100

    Updated code style

--- server.js
+++ server.js
@@ -2,28 +2,21 @@ import express from 'express';
 import expressHandlebars from 'express-handlebars';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-
-
 var app = express();
-
 app.use(cookieParser());
-
 app.use(express.static('build'));
-
 // set up handlebars
 app.engine('handlebars', expressHandlebars({
-                                           defaultLayout: 'main',
-                                           layoutsDir: path.join(__dirname, 'views/layouts'),
-                                           partialsDir: path.join(__dirname, 'views/partials')
-                                         }));
+  defaultLayout: 'main',
+  layoutsDir: path.join(__dirname, 'views/layouts'),
+  partialsDir: path.join(__dirname, 'views/partials')
+}));
 app.set('view engine', 'handlebars');
-
 app.get('/', function(req, res){
-    res.status(200).render('content', {
-      needToRenderFast: !(req.cookies.fastweb === 'cache')
-    });
+  res.status(200).render('content', {
+    needToRenderFast: !(req.cookies.fastweb === 'cache')
+  });
 });
-
 app.listen(3000, function(){
 console.log('Local server is listening...');
-}); 
+});
